99683f10b9c567de766563c7e349e76936bb7c07
rachael <rachael.coder@gmail.com>
2020-02-16 15:08:13 -0800

    navLinks updated

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
 
+const navLinks = [
+  { title: 'About Me', path: '/about' },
+  { title: 'My Projects', path: '/projects' },
+  { title: 'Contact Me', path: '/contactme' }
+]
 
 const Header = () => (
     <div className='header'>
       <div className='options'>
-        <Link className='option' to='/about'>
-          About Me
+        { navLinks.map((obj) => (
+        <Link className='option' to={obj.path}>
+          {obj.title}
         </Link>
-        <Link className='option' to='/projects'>
-          My Projects
-        </Link>
-        <Link className='option' to='/contactme'>
-          Contact Me
-        </Link> 
+        ))}
       </div>
     </div>
   );
